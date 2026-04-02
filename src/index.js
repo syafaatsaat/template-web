78e6f8f8d8b082b758faeb2ebba413faee3228d2
syafaatsaat <47024629+syafaatsaat@users.noreply.github.com>
2026-04-02 17:54:26 +0800

    Create basic files and configurations

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { greeting } from "./greeting.js";
+
+console.log(greeting);
+
+// import odinImage from "./odin.png";
+
+// const image = document.createElement("img");
+// image.src = odinImage;
+
+// document.body.appendChild(image);
