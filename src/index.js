159cda7507eddff400e5c7714287b3c845f8a688
syafaatsaat <47024629+syafaatsaat@users.noreply.github.com>
2026-04-02 18:28:09 +0800

    Split webpack config files and add more commands

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
+
 console.log(greeting);
 
 // import odinImage from "./odin.png";
--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,21 @@
-import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from 'node:path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(import.meta.dirname, "dist"),
-        clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
+    entry: {
+        app: './src/index.js',
     },
     plugins: [
         new HtmlWebpackPlugin({
+            title: 'Production',
             template: "./src/template.html",
         }),
     ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(import.meta.dirname, 'dist'),
+        clean: true,
+    },
     module: {
         rules: [
             {
